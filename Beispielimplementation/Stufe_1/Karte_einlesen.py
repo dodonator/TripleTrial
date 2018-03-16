4a6612ce8ee2fba180e5fa1dbba6dac42f876ed8
dodo <dodo@chaosdorf.de>
2018-03-16 09:51:08 +0100

    Kommentare

--- Beispielimplementation/Stufe_1/Karte_einlesen.py
+++ Beispielimplementation/Stufe_1/Karte_einlesen.py
@@ -3,21 +3,26 @@
 print("Triple Triad")
 print("Karte erstellen")
 
+# Die vier Werte der Karte werden der Reihe nach eingegeben:
 up = input("Bitte den Wert (1-10) für die obere Seite eingeben: ")
 down = input("Bitte den Wert (1-10) für die untere Seite eingeben: ")
 left = input("Bitte den Wert (1-10) für die linke Seite eingeben: ")
 right = input("Bitte den Wert (1-10) für die rechte Seite eingeben: ")
 
+# Die Breite einer Kartendarstellung
 width = 9
 
-s_u = width // 2 - len(up) + 1
-s_lr = width - (len(left) + len(right))
-s_d = width // 2 - len(down) + 1
+# Die Anzahl an Leerzeichen in den jeweiligen Zeilen
+s_u = width // 2 - len(up) + 1  # s_u entspricht "spaces up"
+s_lr = width - (len(left) + len(right))  # s_lr entspricht "space left/right"
+s_d = width // 2 - len(down) + 1  # s_d entspricht "spaces down"
 
-print("+" + width*"-" + "+")
+# Das Ausdrucken der einzelnen Zeilen
+print("+" + width*"-" + "+")  # Kopfzeile
 print("|" + s_u*" " + up + (width//2)*" " + "|")
-print("|" + width*" " + "|")
+print("|" + width*" " + "|")  # "leere" Zeile
 print("|" + left + s_lr*" " + right + "|")
-print("|" + width*" " + "|")
+print("|" + width*" " + "|")  # "leere" Zeile
 print("|" + s_d*" " + down + (width//2)*" " + "|")
-print("+" + width*"-" + "+")
+print("+" + width*"-" + "+") # FußZeilen
+print()  # Leerzeile
